5d00abb3574e230c2fc10911563daf83a725a21a
David <olayinkadavid62@yahoo.com>
2023-11-11 04:30:15 +0100

    complete carCard and CardDetails components

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,10 +3,15 @@
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-const CustomButton = (
-  { title, containerStyles, handleClick, btnType }
-  : CustomButtonProps
-) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+  isDisabled,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
@@ -14,7 +19,17 @@ const CustomButton = (
       onClick={handleClick}
       className={`custom-btn ${containerStyles}`}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
